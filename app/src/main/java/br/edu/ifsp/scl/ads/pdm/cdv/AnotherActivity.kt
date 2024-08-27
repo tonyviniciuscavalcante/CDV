9d92a43a78472b7bbe7bb805800c312ad8430c3c
Tony Vinicius Cavalcante <82682652+tonyviniciuscavalcante@users.noreply.github.com>
2024-08-26 22:04:59 -0300

    Update AnotherActivity.kt

--- app/src/main/java/br/edu/ifsp/scl/ads/pdm/cdv/AnotherActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/cdv/AnotherActivity.kt
@@ -1,6 +1,7 @@
 package br.edu.ifsp.scl.ads.pdm.cdv
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +10,37 @@ import androidx.core.view.WindowInsetsCompat
 class AnotherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_another)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        Log.v(getString(R.string.app_name), "onCreate-A: Iniciando ciclo completo")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.v(getString(R.string.app_name), "onStart-A: Iniciando ciclo visível")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.v(getString(R.string.app_name), "onResume-A: Iniciando ciclo foreground")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.v(getString(R.string.app_name), "onPause-A: Finalizando ciclo foreground")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.v(getString(R.string.app_name), "onStop-A: Finalizando ciclo visível")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.v(getString(R.string.app_name), "onDestroy-A: Finalizando ciclo completo")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.v(getString(R.string.app_name), "onRestart-A: Preparando onStart")
     }
 }
