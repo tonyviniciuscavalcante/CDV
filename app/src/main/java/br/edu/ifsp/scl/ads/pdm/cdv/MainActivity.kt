50e03fcba4dcbe2610c51c286a82610565ed5225
Tony Vinicius Cavalcante <82682652+tonyviniciuscavalcante@users.noreply.github.com>
2024-08-26 21:35:28 -0300

    Update MainActivity.kt

--- app/src/main/java/br/edu/ifsp/scl/ads/pdm/cdv/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/cdv/MainActivity.kt
@@ -1,20 +1,21 @@
 package br.edu.ifsp.scl.ads.pdm.cdv
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.scl.ads.pdm.cdv.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(amb.root)
+        Log.v(getString(R.string.app_name), "onCreate: Iniciando ciclo completo")
     }
 }
