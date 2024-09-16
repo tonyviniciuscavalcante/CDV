9e34ee2ce66b4023d7fdb9b87ac07cd0109868cf
Tony Vinicius Cavalcante <82682652+tonyviniciuscavalcante@users.noreply.github.com>
2024-09-16 12:16:01 -0300

    AulaCompleta09-09

--- app/src/main/java/br/edu/ifsp/scl/ads/pdm/cdv/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/cdv/MainActivity.kt
@@ -3,6 +3,10 @@ package br.edu.ifsp.scl.ads.pdm.cdv
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.EditText
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,46 +18,92 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private companion object {
+        const val VALOR_TELEFONE_UM = "VALOR_TELEFONE_UM"
+        const val VALOR_TELEFONE_DOIS = "VALOR_TELEFONE_DOIS"
+
+    }
+
+    private val telefoneDoisEt: EditText by lazy {
+        EditText(this)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
 
-        amb.abraBt.setOnClickListener {
+        telefoneDoisEt.hint = "Segundo telefone"
+        telefoneDoisEt.layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
+        amb.root.addView(telefoneDoisEt)
+
+        savedInstanceState?.apply {
+            getString(VALOR_TELEFONE_UM)?.let {
+                amb.telefoneUmEt.setText(it)
+            }
+            getString(VALOR_TELEFONE_DOIS, "0000000000").let {
+                telefoneDoisEt.setText(it)
+            }
+            Log.v(getString(R.string.app_name),"onSaveRestoreState: Restaurando dados de instancia")
+        }
+
+        amb.abraBt.setOnClickListener{
             Intent(this, AnotherActivity::class.java).also {
                 startActivity(it)
             }
+
         }
 
-        Log.v(getString(R.string.app_name), "onCreate: Iniciando ciclo completo")
+        Log.v(getString(R.string.app_name), "onCreate: iniciando ciclo completo")
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(VALOR_TELEFONE_UM, amb.telefoneUmEt.text.toString())
+        outState.putString(VALOR_TELEFONE_DOIS, telefoneDoisEt.text.toString())
+        Log.v(getString(R.string.app_name),"onSaveInstaceState: Salvando dados de instancia")
+    }
+
+    /*override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        savedInstanceState.apply {
+            getString(VALOR_TELEFONE_UM)?.let {
+                amb.telefoneUmEt.setText(it)
+            }
+            getString(VALOR_TELEFONE_DOIS, "0000000000").let {
+                telefoneDoisEt.setText(it)
+            }
+        }
+        Log.v(getString(R.string.app_name),"onSaveRestoreState: Restaurando dados de instancia")
+    }*/
 
     override fun onStart() {
         super.onStart()
-        Log.v(getString(R.string.app_name), "onStart: Iniciando ciclo visível")
+        Log.v(getString(R.string.app_name), "onStart: iniciando ciclo visivel")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.v(getString(R.string.app_name), "onResume: Iniciando ciclo foreground")
+        Log.v(getString(R.string.app_name), "onResume: iniciando ciclo foreground")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.v(getString(R.string.app_name), "onPause: Finalizando ciclo foreground")
+        Log.v(getString(R.string.app_name), "onPause: finalizandos ciclo foreground")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.v(getString(R.string.app_name), "onStop: Finalizando ciclo visível")
+        Log.v(getString(R.string.app_name), "onStop: finalizando ciclo visivel")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.v(getString(R.string.app_name), "onDestroy: Finalizando ciclo completo")
+        Log.v(getString(R.string.app_name), "onDestroy: finalizando ciclo completo")
     }
 
     override fun onRestart() {
         super.onRestart()
-        Log.v(getString(R.string.app_name), "onRestart: Preparando onStart")
+        Log.v(getString(R.string.app_name), "onRestart: preparando onStart")
     }
 }
